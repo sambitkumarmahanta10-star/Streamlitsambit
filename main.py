d92f999643a4684b9d99edcd374a687d7290012f
sambitkumarmahanta10-star <sambitkumarmahanta10@gmail.com>
2026-04-28 08:45:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ if st.button("submit"):
   else:
     st.write("u cannot vote")
 st.caption("smol  text")
-st.latex(r'''
+st.write(st.latex(r'''
     a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
     \sum_{k=0}^{n-1} ar^k =
     a \left(\frac{1-r^{n}}{1-r}\right)
-    ''')
+    '''))
