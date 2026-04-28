4bfadabf7298470f90594f6f141eb46952671eec
sambitkumarmahanta10-star <sambitkumarmahanta10@gmail.com>
2026-04-28 08:17:19 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.title("streamsambitlit")
+st.write("hallo, use streamlit")
+
+name= st.text_input(" enyter name")
+age = st.number_input("your age")
+
+if st.button("submit"):
+  st.write("hallo,{name}! welcome to streamlit")
