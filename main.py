0f70382d29513f59925478e097423263cd29ad20
sambitkumarmahanta10-star <sambitkumarmahanta10@gmail.com>
2026-04-28 08:31:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,5 +9,7 @@ if st.button("submit"):
   st.write(f"hallo,{name}! welcome to streamlit")
   if age >= 18:
     st.write("u can vote")
+  elif age >= 65:
+    st.write("u dead")
   else:
     st.write("u cannot vote")
